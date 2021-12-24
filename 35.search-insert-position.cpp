e0a7b234dfa104b5e2b6ff199fd9cc036ffeae3b
Tianli Tong <tongtianli@126.com>
2021-12-23 17:16:30 -0800

    35

--- 35.search-insert-position.cpp
+++ 35.search-insert-position.cpp
@@ -9,15 +9,15 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int n=nums.size(), left=0, right=n-1;
-        while(left<right){
-            int mid = left + (right-left)>>1;
-            if(nums[right]<=nums[mid]){
-                right = mid;
+        while(left<=right){
+            int mid = left + ((right-left)>>1);
+            if(target<=nums[mid]){
+                right = mid-1;
             }else{
                 left = mid+1;
             }
         }
-        return right;
+        return right+1;
     }
 };
 // @lc code=end

07b296f860cf1ae1bfff5e16bf045bb3e26b902e
Tianli Tong <tongtianli@126.com>
2021-12-28 11:11:03 -0800

    113

---
+++ 113.path-sum-ii.cpp
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode id=113 lang=cpp
+ *
+ * [113] Path Sum II
+ *
+ * https://leetcode.com/problems/path-sum-ii/description/
+ *
+ * algorithms
+ * Medium (52.77%)
+ * Likes:    4061
+ * Dislikes: 98
+ * Total Accepted:    522.4K
+ * Total Submissions: 989.5K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+ *
+ * Given the root of a binary tree and an integer targetSum, return all
+ * root-to-leaf paths where the sum of the node values in the path equals
+ * targetSum. Each path should be returned as a list of the node values, not
+ * node references.
+ * 
+ * A root-to-leaf path is a path starting from the root and ending at any leaf
+ * node. A leaf is a node with no children.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * Output: [[5,4,11,2],[5,8,4,5]]
+ * Explanation: There are two paths whose sum equals targetSum:
+ * 5 + 4 + 11 + 2 = 22
+ * 5 + 8 + 4 + 5 = 22
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,2,3], targetSum = 5
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [1,2], targetSum = 0
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> results;
+        if(root){
+            vector<int> path={root->val};
+        
+            findPath(results, path, root, targetSum);
+        }
+        
+        return results;
+    }
+    
+    void findPath(vector<vector<int>> &results, vector<int> &path, TreeNode *p, int remainSum){
+        if(p->left==NULL && p->right==NULL && remainSum==p->val){
+            results.push_back(path);
+            return;
+        }
+        
+        remainSum -= p->val;
+        
+        if(p->left){
+            path.push_back(p->left->val);
+            findPath(results, path, p->left, remainSum);
+            path.pop_back();
+        }
+        if(p->right){
+            path.push_back(p->right->val);
+            findPath(results, path, p->right, remainSum);
+            path.pop_back();
+        }
+        
+    }
+};
+// @lc code=end
+

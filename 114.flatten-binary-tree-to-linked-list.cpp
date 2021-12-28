590b971c911226dffe713cb9065fa6ad8866402b
Tianli Tong <tongtianli@126.com>
2021-12-28 11:21:56 -0800

    114

---
+++ 114.flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=114 lang=cpp
+ *
+ * [114] Flatten Binary Tree to Linked List
+ *
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (56.27%)
+ * Likes:    6101
+ * Dislikes: 439
+ * Total Accepted:    543.9K
+ * Total Submissions: 966K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * Given the root of a binary tree, flatten the tree into a "linked
+ * list":
+ * 
+ * 
+ * The "linked list" should use the same TreeNode class where the right child
+ * pointer points to the next node in the list and the left child pointer is
+ * always null.
+ * The "linked list" should be in the same order as a pre-order traversal of
+ * the binary tree.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,2,5,3,4,null,6]
+ * Output: [1,null,2,null,3,null,4,null,5,null,6]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = [0]
+ * Output: [0]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root) return;
+        
+        TreeNode *temp = root->right;
+        root->right = root->left;
+        root->left=NULL;
+        
+        flatten(root->right);
+        flatten(temp);
+        
+        TreeNode *p=root;
+        while(p->right) p=p->right;
+        p->right = temp;
+        
+    }
+};
+// @lc code=end
+

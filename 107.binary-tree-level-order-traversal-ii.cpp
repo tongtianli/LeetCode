c9c1a68e09c15242c29ece9062ca86a796d866e8
Tianli Tong <tongtianli@126.com>
2021-12-29 11:48:13 -0800

    107

---
+++ 107.binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=107 lang=cpp
+ *
+ * [107] Binary Tree Level Order Traversal II
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+ *
+ * algorithms
+ * Medium (57.55%)
+ * Likes:    2771
+ * Dislikes: 265
+ * Total Accepted:    466.3K
+ * Total Submissions: 809.9K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return the bottom-up level order traversal
+ * of its nodes' values. (i.e., from left to right, level by level from leaf to
+ * root).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[15,7],[9,20],[3]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: [[1]]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -1000 <= Node.val <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        if(root==NULL){
+            return res;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> level;
+            for(int i=0;i<size;i++){
+                TreeNode *cur=q.front();
+                q.pop();
+                level.push_back(cur->val);
+                if(cur->left) q.push(cur->left);
+                if(cur->right) q.push(cur->right);
+            }
+            res.push_back(level);
+        }
+        
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
+
+// @lc code=end
+

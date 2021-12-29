4f8f8ce46302cbd1ef7c0020256d9797ae63ef97
Tianli Tong <tongtianli@126.com>
2021-12-29 11:49:44 -0800

    217

---
+++ 217.contains-duplicate.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=217 lang=cpp
+ *
+ * [217] Contains Duplicate
+ *
+ * https://leetcode.com/problems/contains-duplicate/description/
+ *
+ * algorithms
+ * Easy (59.52%)
+ * Likes:    3248
+ * Dislikes: 905
+ * Total Accepted:    1.2M
+ * Total Submissions: 2M
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * Given an integer array nums, return true if any value appears at least twice
+ * in the array, and return false if every element is distinct.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ * Example 2:
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ * Example 3:
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s(nums.begin(), nums.end());
+        
+        return s.size()!=nums.size();
+    }
+};
+// @lc code=end
+

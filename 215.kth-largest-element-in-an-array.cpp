439ad9722b0225751a064267e37a10d391c76e07
Tianli Tong <tongtianli@126.com>
2022-01-09 16:33:07 -0800

    215

---
+++ 215.kth-largest-element-in-an-array.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=215 lang=cpp
+ *
+ * [215] Kth Largest Element in an Array
+ *
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+ *
+ * algorithms
+ * Medium (61.96%)
+ * Likes:    7832
+ * Dislikes: 439
+ * Total Accepted:    1.1M
+ * Total Submissions: 1.8M
+ * Testcase Example:  '[3,2,1,5,6,4]\n2'
+ *
+ * Given an integer array nums and an integer k, return the k^th largest
+ * element in the array.
+ * 
+ * Note that it is the k^th largest element in the sorted order, not the k^th
+ * distinct element.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [3,2,1,5,6,4], k = 2
+ * Output: 5
+ * Example 2:
+ * Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+ * Output: 4
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= k <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+// Sort, 17.5% 57.83%
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()-k];
+    }
+};
+
+// Quick select, 
+// @lc code=end
+
